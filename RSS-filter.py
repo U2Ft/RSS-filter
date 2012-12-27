b9a936e7f5283e92052192bd34e95ad149f04916
Sam Mellor <github@u2ft.me>
2012-12-27 15:20:39 +1100

    implemented config file

--- RSS-filter.py
+++ RSS-filter.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python2
 
+
+import os
+import errno
+import stat
+from cStringIO import StringIO
+
 import libgreader
 import requests
+import configobj
+import validate
+import appdirs
 
 
 def short_link(URL):
@@ -22,17 +31,18 @@ class GoogleReader:
 
         if refresh_token:
             self.refresh_token = refresh_token
-        else:
-            self.refresh_token = self._get_refresh_token()
-
-        self.auth = self._authenticate()
-        self.libgreader = libgreader.GoogleReader(self.auth)
+            self.auth = self._authenticate()
+            self.libgreader = libgreader.GoogleReader(self.auth)
 
     def _authenticate(self):
         payload = {"client_id": self.client_ID, "client_secret": self.client_secret,
                    "refresh_token": self.refresh_token, "grant_type": "refresh_token"}
         r = requests.post("https://accounts.google.com/o/oauth2/token", data=payload)
-        access_token = r.json["access_token"]
+        try:
+            access_token = r.json["access_token"]
+        except KeyError as e:
+            print "\n{}\n\n{}\n\nError. Couldn't parse access token.".format(e, r.text)
+            exit(3)
 
         auth = libgreader.OAuth2Method(self.client_ID, self.client_secret)
         auth.setRedirectUri("urn:ietf:wg:oauth:2.0:oob")
@@ -40,7 +50,7 @@ class GoogleReader:
 
         return auth
 
-    def _get_refresh_token(self):
+    def get_refresh_token(self):
         auth = libgreader.OAuth2Method(self.client_ID, self.client_secret)
         auth.setRedirectUri("urn:ietf:wg:oauth:2.0:oob")
         auth_URL = short_link(auth.buildAuthUrl())
@@ -60,10 +70,44 @@ class GoogleReader:
         return self.libgreader.getUserInfo()
 
 
+def check_config(config_dir):
+    try:
+        os.makedirs(config_dir)
+    except OSError as e:
+        if e.errno == errno.EEXIST and os.path.isdir(config_dir):
+            pass
+        else:
+            raise
+
+    spec = """client_ID = string\nclient_secret = string\nrefresh_token = string"""
+    config_spec = configobj.ConfigObj(StringIO(spec))
+    config_path = os.path.join(config_dir, "settings.ini")
+    config = configobj.ConfigObj(config_path, configspec=config_spec)
+    valid = config.validate(validate.Validator())
+
+    if valid is True:
+        return config
+    elif valid is False:
+        config["client_ID"] = raw_input("Google OAuth Client ID\n:  ")
+        config["client_secret"] = raw_input("Google OAuth Client Secret\n:  ")
+        GR = GoogleReader(config["client_ID"], config["client_secret"], None)
+        config["refresh_token"] = GR.get_refresh_token()
+
+    try:
+        config.write()
+        os.chmod(config_path, stat.S_IRUSR | stat.S_IWUSR)  # mode -rw-------
+        print "Config written successfully."
+        exit(1)
+    except Exception as e:
+        print "{}\nConfig file was not written.".format(e)
+        exit(2)
+
+
 def main():
-    client_ID = raw_input("Google OAuth Client ID\n:  ")
-    client_secret = raw_input("Google OAuth Client Secret\n:  ")
-    reader = GoogleReader(client_ID, client_secret, None)
+    config_dir = appdirs.user_data_dir("RSS-filter", "U2Ft")
+    config = check_config(config_dir)
+
+    reader = GoogleReader(config["client_ID"], config["client_secret"], config["refresh_token"])
 
     print reader.user_info()
 
