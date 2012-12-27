7f37d246b09b74a2c486a23d5fced2689d33c4f7
Sam Mellor <github@u2ft.me>
2012-12-27 14:19:30 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RSS Filter
+
+Marks-as-read Google Reader feed items that match a specified filter.
---
+++ RSS-filter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+
+import libgreader
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
