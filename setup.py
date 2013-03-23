84092e74fc9dccb8c2e169f2ff27e49134107e75
Sam Mellor <github@u2ft.me>
2013-03-24 03:02:39 +1100

    Bump version to 1.0.0
    Also added some metadata.

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
+import os
 from setuptools import setup
 
 setup(
     name="RSS-filter",
-    version="0.0.1",
+    version="1.0.0",
+    description="Marks-as-read Google Reader feed items that match a specified filter.",
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    url="https://github.com/U2Ft/RSS-filter",
+    license="MIT",
     package_dir={"": "src"},
     py_modules=["RSS_filter"],
     install_requires=["libgreader", "requests>=1.0.0", "configobj", "appdirs", "docopt", "demjson"],
     zip_safe=True,
-    entry_points = {"console_scripts": ["RSS-filter = RSS_filter:main"]}
+    entry_points={"console_scripts": ["RSS-filter = RSS_filter:main"]}
 )
